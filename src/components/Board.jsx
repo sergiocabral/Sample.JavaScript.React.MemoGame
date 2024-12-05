56a15447ec5c26eb4295e0c86763fc4d11593fdb
Sergio Cabral <git@sergiocabral.com>
2024-12-05 20:00:00 -0300

    Estilos CSS

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,10 +3,20 @@ import { Card } from "./Card.jsx";
 
 export function Board() {
   return (
-    <div>
+    <div style={style}>
       <Card />
       <Card />
       <Card />
     </div>
   )
+}
+
+const style = {
+  backgroundColor: '#2f2f2f',
+  flexGrow: 1,
+  display: 'grid',
+  gridTemplateColumns: 'repeat(5, 1fr)',
+  gridTemplateRows: 'repeat(5, 1fr)',
+  gap: '1em',
+  padding: '1em',
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 export function Card() {
   return (
-    <button>
+    <button style={style}>
       Card
     </button>
   )
+}
+
+const style = {
+  backgroundColor: '#676767',
+  fontSize: '3em',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: '16px',
+  border: '1px solid #1a1a1a',
+  cursor: 'pointer',
 }
