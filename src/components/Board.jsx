548c1866d91ff4e428677306d80cd5384cfa8f01
Sergio Cabral <git@sergiocabral.com>
2024-12-05 20:00:00 -0300

    renderizando listas

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Card } from './Card'
 
+const icons = [
+    '🐳', '😈', '🤡', '🧐',
+    '🤢', '🙊', '🐸', '🐼',
+]
+
 export function Board() {
     return (
         <div style={style}>
-            <Card>😂</Card>
-            <Card>😈</Card>
-            <Card>🐳</Card>
+            {[...icons, ...icons].map((icon, index) => (
+                <Card key={index}>{icon}</Card>
+            ))}
         </div>
     )
 }
@@ -15,8 +20,8 @@ const style = {
     backgroundColor: '#2f2f2f',
     flexGrow: 1,
     display: 'grid',
-    gridTemplateColumns: 'repeat(5, 1fr)',
-    gridTemplateRows: 'repeat(5, 1fr)',
+    gridTemplateColumns: 'repeat(4, 1fr)',
+    gridTemplateRows: 'repeat(4, 1fr)',
     gap: '1em',
     padding: '1em',
 }
