42c6b4c92d89123a47368dcfd82da8781bab9adb
Sergio Cabral <git@sergiocabral.com>
2024-12-05 20:00:00 -0300

    Configurado babel e webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('node:path')
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    devServer: {
+        static: './dist'
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jss',
+        ]
+    }
+}
