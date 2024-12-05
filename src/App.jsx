77207283e17b163266b4fb1d1c414ae89fe807f4
Sergio Cabral <git@sergiocabral.com>
2024-12-05 20:00:00 -0300

    Componentes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Board } from "./components/Board";
+
+export function App() {
+  return (
+    <div>
+      <h1>Jogo da Memória</h1>
+      <Board />
+    </div>
+  )
+}
---
+++ src/components/Board.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from "./Card.jsx";
+
+export function Board() {
+  return (
+    <div>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+}
