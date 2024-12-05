6944ac791e81bf4c00986a505256fdcc5ff10d42
Sergio Cabral <git@sergiocabral.com>
2024-12-05 20:00:00 -0300

    Sincronizando Comportamentos (useEffect)

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Card } from "./Card.jsx";
 
 const icons = [
@@ -19,13 +19,46 @@ function getCards() {
 }
 
 export function Board() {
+  const [verifying, setVerifying] = useState(false)
   const [cards, setCards] = useState(getCards())
 
   function onClick(card) {
+    if (verifying || card.selected || card.matched) return
+
     const cardIndex = cards.findIndex(c => c.index == card.index)
     cards[cardIndex].showing = !cards[cardIndex].showing
+
+    cards[cardIndex].selected = true
+    const selecteds = cards.filter(card => card.selected)
+    if (selecteds.length === 2) {
+      setVerifying(true)
+    }
+
     setCards([...cards])
   }
+
+  useEffect(() => {
+    if (!verifying) return
+
+    setTimeout(() => {
+      setVerifying(false)
+
+      const selecteds = cards.filter(card => card.selected)
+
+      selecteds[0].selected = false
+      selecteds[1].selected = false
+      if (selecteds[0].icon === selecteds[1].icon) {
+        selecteds[0].matched = true
+        selecteds[1].matched = true
+      } else {
+        selecteds[0].showing = false
+        selecteds[1].showing = false
+      }
+
+      setCards([...cards])
+    }, 1000)
+
+  }, [verifying])
 
   return (
     <div style={style}>
